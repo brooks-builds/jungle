3f2b434b5d97fce715bbe4b192178f9c2fab905e
Brooks Patton <brooks@brookzerker.com>
2020-11-10 09:19:30 -0700

    Figuring out how to draw to the canvas

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,37 @@
+use js_sys::Function;
 use std::f64;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
+use web_sys::*;
 
 #[wasm_bindgen]
 extern "C" {}
 
+pub fn set_panic_hook() {
+    // When the `console_error_panic_hook` feature is enabled, we can call the
+    // `set_panic_hook` function at least once during initialization, and then
+    // we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    // #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
+}
+
 #[wasm_bindgen]
-pub fn run() {
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas = document.get_element_by_id("canvas").unwrap();
-    let canvas: web_sys::HtmlCanvasElement = canvas
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .unwrap();
+pub struct GameState {
+    image_url: String,
+}
 
-    let context = canvas
-        .get_context("2d")
-        .unwrap()
-        .unwrap()
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
-        .unwrap();
+#[wasm_bindgen]
+impl GameState {
+    pub fn setup() -> Self {
+        GameState {
+            image_url: "./KawaiiIcons_NoBG037.png".to_owned(),
+        }
+    }
 
-    context.begin_path();
-
-    // Draw the outer circle.
-    context
-        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the mouth.
-    context.move_to(110.0, 75.0);
-    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
-
-    // Draw the left eye.
-    context.move_to(65.0, 65.0);
-    context
-        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    // Draw the right eye.
-    context.move_to(95.0, 65.0);
-    context
-        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
-        .unwrap();
-
-    context.stroke();
+    pub fn get_image_url(&self) -> String {
+        self.image_url.clone()
+    }
 }
--- www/main.js
+++ www/main.js
@@ -1,24 +1,27 @@
 import * as wasm from 'jungle';
 import config from './config.json';
 
+const hello = 'hello world';
+
 // setup
-let canvas = document.querySelector('canvas');
+let gameState = wasm.GameState.setup();
+let canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 const width = canvas.width;
 const height = canvas.height;
 const image = new Image(512, 512);
-image.src = "./KawaiiIcons_NoBG037.png";
+image.src = gameState.get_image_url();
+console.log(gameState.get_image_url())
 
-// // game loop
-// requestAnimationFrame(loop);
+// game loop
+requestAnimationFrame(loop);
 
-// function loop() {
-//     run
-//     requestAnimationFrame(loop);
-// }
+function loop() {
+    // run
+    draw()
+    requestAnimationFrame(loop);
+}
 
-// function draw() {
-//     context.drawImage(image, 0, 0, 10, 10);
-// }
-
-wasm.run();
+function draw() {
+    context.drawImage(image, 0, 0, 10, 10);
+}
