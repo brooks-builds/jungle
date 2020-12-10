a3ccbd22c39e87d4c6a664445b23a8232bb0da16
Brooks Patton <brooks@brookzerker.com>
2020-12-09 20:27:41 -0700

    now just building on the pi

--- launcher.sh
+++ launcher.sh
@@ -4,16 +4,8 @@
 
 VERSION_FILE="$HOME/jungle_version"
 DEPLOYED_VERSION_URL="https://brooks-builds-games.s3-us-west-1.amazonaws.com/jungle/version"
-GAME_URL="https://brooks-builds-games.s3-us-west-1.amazonaws.com/jungle/jungle_game.zip"
-GAME_DIRECTORY="$HOME/jungle"
-
-if [ -d $GAME_DIRECTORY ]
-then 
-    cd "$GAME_DIRECTORY" || exit
-else
-    mkdir "$GAME_DIRECTORY"
-    cd "$GAME_DIRECTORY" || exit
-fi
+REPO="$HOME/jungle_repo"
+TARGET_RESOURCES="$REPO/target/release/resources"
 
 if [ -f $VERSION_FILE ]
 then
@@ -22,17 +14,18 @@ else
     echo "0.0.0" > $VERSION_FILE
     CURRENT_VERSION="0.0.0"
 fi
-echo $CURRENT_VERSION
 
 DEPLOYED_VERSION=$(curl $DEPLOYED_VERSION_URL)
 
-echo current: $CURRENT_VERSION deployed: $DEPLOYED_VERSION
 if [ "$CURRENT_VERSION" != "$DEPLOYED_VERSION" ]
 then
-    curl $GAME_URL > jungle.zip
-    unzip -o jungle.zip
+    git pull
+    if [ -d $TARGET_RESOURCES ]
+    then
+        rm -rf $TARGET_RESOURCES
+    fi
+    cp -r resources $TARGET_RESOURCES
     echo $DEPLOYED_VERSION > $VERSION_FILE
-    cp target/release/jungle ./
 fi
     
-./jungle
+cargo run --release
